abe7970afdcdbcc46f554e9742ccb565596b465a
rafirahman123 <76124938+rafirahman123@users.noreply.github.com>
2021-09-26 05:28:11 +0600

    Last change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header">
-            <h2 className="header-title">Capston projects</h2>
+            <h2 className="header-title">Capston projects group</h2>
             <p>Capston project is a final year project for CSE student.It is a combine course for thesis and project together.</p>
             <h3>Total Weiver Budget for wach group: 50000</h3>
         </div>
